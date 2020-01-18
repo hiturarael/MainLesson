a8d465156fc2ed8b798d4e06c8c5b9924f5ed14d
hiturarael <hiturarael@hotmail.com>
2020-01-18 13:46:34 -0500

    lesson 1 fin

--- Lesson1/HelloWorld.Designer.cs
+++ Lesson1/HelloWorld.Designer.cs
@@ -28,9 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HelloForm));
             this.MessageLabel = new System.Windows.Forms.Label();
             this.PushButton = new System.Windows.Forms.Button();
             this.ExitButton = new System.Windows.Forms.Button();
+            this.SpanishButton = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // MessageLabel
@@ -43,31 +47,66 @@
             // 
             // PushButton
             // 
-            this.PushButton.Location = new System.Drawing.Point(159, 218);
+            this.PushButton.Location = new System.Drawing.Point(47, 217);
             this.PushButton.Name = "PushButton";
             this.PushButton.Size = new System.Drawing.Size(95, 36);
             this.PushButton.TabIndex = 1;
-            this.PushButton.Text = "Push Me";
+            this.PushButton.Text = "English";
             this.PushButton.UseVisualStyleBackColor = true;
+            this.PushButton.Click += new System.EventHandler(this.PushButton_Click);
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(159, 260);
+            this.ExitButton.Location = new System.Drawing.Point(275, 259);
             this.ExitButton.Name = "ExitButton";
             this.ExitButton.Size = new System.Drawing.Size(95, 36);
             this.ExitButton.TabIndex = 2;
             this.ExitButton.Text = "Exit";
             this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // SpanishButton
+            // 
+            this.SpanishButton.Location = new System.Drawing.Point(47, 259);
+            this.SpanishButton.Name = "SpanishButton";
+            this.SpanishButton.Size = new System.Drawing.Size(95, 36);
+            this.SpanishButton.TabIndex = 3;
+            this.SpanishButton.Text = "Spanish";
+            this.SpanishButton.UseVisualStyleBackColor = true;
+            this.SpanishButton.Click += new System.EventHandler(this.SpanishButton_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(263, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Programmed by Hitura Rael";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // HelloForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(413, 414);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SpanishButton);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.PushButton);
             this.Controls.Add(this.MessageLabel);
             this.Name = "HelloForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hello World";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -79,6 +118,9 @@
         private System.Windows.Forms.Label MessageLabel;
         private System.Windows.Forms.Button PushButton;
         private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button SpanishButton;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ Lesson1/HelloWorld.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson1
+{
+    public partial class HelloForm : Form
+    {
+        public HelloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void PushButton_Click(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "Hello World!";
+        }
+
+
+        private void SpanishButton_Click(object sender, EventArgs e)
+        {
+            MessageLabel.Text = "Hola Mundo";
+        }
+    }
+}
